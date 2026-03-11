b482eeac18c860243ce1f10c16695cfcc6222445
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2026-03-11 20:13:44 +0200

    Fix containerof to be standards compliant

--- misc/containerof.h
+++ misc/containerof.h
@@ -1,7 +1,9 @@
 #ifndef _CONTAINEROF_H_
 #define _CONTAINEROF_H_
 
+#include <stddef.h>
+
 #define CONTAINER_OF(ptr, type, member) \
-  ((type*)(((char*)ptr) - (((char*)&(((type*)0)->member)) - ((char*)0))))
+  ((type*)(((char*)ptr) - offsetof(type, member)))
 
 #endif
